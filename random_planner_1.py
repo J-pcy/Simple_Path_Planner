42f9e15b232ebacfc518c4b337d8b5b5735cec21
Chenyu <chenyupe@usc.edu>
2018-07-05 10:13:44 -0700

    Simple_Path_Planner

--- random_planner_1.py
+++ random_planner_1.py
@@ -3,25 +3,25 @@ import numpy as np
 max_step_number = 100
 
 def search(world_state, robot_pose, goal_pose):
-	if world_state[robot_pose[0]][robot_pose[1]]==1:
+	if world_state[robot_pose[0]][robot_pose[1]] == 1:
 		return None
-	if world_state[goal_pose[0]][goal_pose[1]]==1:
+	if world_state[goal_pose[0]][goal_pose[1]] == 1:
 		return None
 	path = [robot_pose]
 	short_memory = [robot_pose]
-	while len(path)<=max_step_number:
+	while len(path) <= max_step_number:
 		robot_pose_old = path[-1]
 		rand_num = np.random.randint(0,4)
-		if rand_num==0:
-			robot_pose_new = (robot_pose_old[0]-1, robot_pose_old[1])
-		elif rand_num==1:
-			robot_pose_new = (robot_pose_old[0]+1, robot_pose_old[1])
-		elif rand_num==2:
-			robot_pose_new = (robot_pose_old[0], robot_pose_old[1]-1)
-		elif rand_num==3:
-			robot_pose_new = (robot_pose_old[0], robot_pose_old[1]+1)
+		if rand_num == 0:
+			robot_pose_new = (robot_pose_old[0] - 1, robot_pose_old[1])
+		elif rand_num == 1:
+			robot_pose_new = (robot_pose_old[0] + 1, robot_pose_old[1])
+		elif rand_num == 2:
+			robot_pose_new = (robot_pose_old[0], robot_pose_old[1] - 1)
+		elif rand_num == 3:
+			robot_pose_new = (robot_pose_old[0], robot_pose_old[1] + 1)
 
-		if robot_pose_new[0]<0 or robot_pose_new[0]>len(world_state)-1 or robot_pose_new[1]<0 or robot_pose_new[1]>len(world_state[0])-1 or world_state[robot_pose_new[0]][robot_pose_new[1]]==1:
+		if robot_pose_new[0] < 0 or robot_pose_new[0] > len(world_state) - 1 or robot_pose_new[1] < 0 or robot_pose_new[1] > len(world_state[0]) - 1 or world_state[robot_pose_new[0]][robot_pose_new[1]] == 1:
 			robot_pose_new = robot_pose_old
 
 		if robot_pose_new != robot_pose_old:
--- random_planner_2.py
+++ random_planner_2.py
@@ -4,42 +4,42 @@ import random
 max_step_number = 100
 
 def search(world_state, robot_pose, goal_pose):
-	if world_state[robot_pose[0]][robot_pose[1]]==1:
+	if world_state[robot_pose[0]][robot_pose[1]] == 1:
 		return None
-	if world_state[goal_pose[0]][goal_pose[1]]==1:
+	if world_state[goal_pose[0]][goal_pose[1]] == 1:
 		return None
 	path = [robot_pose]
 	short_memory = [robot_pose]
-	while len(path)<=max_step_number:
+	while len(path) <= max_step_number:
 		robot_pose_old = path[-1]
 		next_pose = []
 		next_pose_new = []
 		pose_in_memory = {}
-		if robot_pose_old[0]-1 >=0 and world_state[robot_pose_old[0]-1][robot_pose_old[1]]!=1:
-			next_pose.append((robot_pose_old[0]-1, robot_pose_old[1]))
-		if robot_pose_old[0]+1 <=len(world_state)-1 and world_state[robot_pose_old[0]+1][robot_pose_old[1]]!=1:
-			next_pose.append((robot_pose_old[0]+1, robot_pose_old[1]))
-		if robot_pose_old[1]-1 >=0 and world_state[robot_pose_old[0]][robot_pose_old[1]-1]!=1:
-			next_pose.append((robot_pose_old[0], robot_pose_old[1]-1))
-		if robot_pose_old[1]+1 <=len(world_state[0])-1 and world_state[robot_pose_old[0]][robot_pose_old[1]+1]!=1:
-			next_pose.append((robot_pose_old[0], robot_pose_old[1]+1))
+		if robot_pose_old[0] - 1 >= 0 and world_state[robot_pose_old[0] - 1][robot_pose_old[1]] != 1:
+			next_pose.append((robot_pose_old[0] - 1, robot_pose_old[1]))
+		if robot_pose_old[0] + 1 <= len(world_state) - 1 and world_state[robot_pose_old[0] + 1][robot_pose_old[1]] != 1:
+			next_pose.append((robot_pose_old[0] + 1, robot_pose_old[1]))
+		if robot_pose_old[1] - 1 >= 0 and world_state[robot_pose_old[0]][robot_pose_old[1] - 1] != 1:
+			next_pose.append((robot_pose_old[0], robot_pose_old[1] - 1))
+		if robot_pose_old[1] + 1 <= len(world_state[0]) - 1 and world_state[robot_pose_old[0]][robot_pose_old[1] + 1] != 1:
+			next_pose.append((robot_pose_old[0], robot_pose_old[1] + 1))
 
-		if len(next_pose) ==0:
+		if len(next_pose) == 0:
 			return None
 		else:
 			for pose in next_pose:
 				flag = 0
-				for i in xrange(len(short_memory)):
+				for i in range(len(short_memory)):
 					if pose == short_memory[i]:
 						flag = 1
 						pose_in_memory[pose] = i
-				if flag ==0:
+				if flag == 0:
 					next_pose_new.append(pose)
 
 			if len(next_pose_new) > 0:
 				robot_pose_new = random.sample(next_pose_new,1)[0]
 			else:
-				robot_pose_new = min(pose_in_memory, key=pose_in_memory.get)
+				robot_pose_new = min(pose_in_memory, key = pose_in_memory.get)
 
 		path.append(robot_pose_new)
 		short_memory.append(robot_pose_new)
